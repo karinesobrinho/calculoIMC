50241bb59829c1a406aec966c5b0267695993535
karine sobrinho <karine.silva.sobrinho@gmail.com>
2020-09-30 17:34:15 -0300

    layout ok

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,9 @@ class _HomeState extends State<Home> {
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0),
+        child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Icon(Icons.person_outline, color: Colors.green[300], size: 120.0),
@@ -44,8 +46,23 @@ class _HomeState extends State<Home> {
           textAlign: TextAlign.center,
           style: TextStyle(fontSize: 25.0, color: Colors.green[300]),
           ),
+          Padding(padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+            child: Container(
+              height: 50.0,
+              child: 
+              RaisedButton(
+              onPressed: (){},
+              child: Text('Calcular', style: TextStyle(color: Colors.white, fontSize: 25.0),),
+              color: Colors.green[300],
+              ),
+            ),
+          ),
+          Text('x',
+          textAlign: TextAlign.center, 
+          style: TextStyle(color: Colors.green[300], fontSize: 25.0),)
         ],
       ),
+      )
     );
   }
 }
